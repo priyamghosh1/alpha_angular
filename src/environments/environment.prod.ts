531d0bb94a5aa5d646e7c43dce387e796cb5be28
priyamghosh1 <priyamghosh73@gmail.com>
2022-12-31 07:57:16 +0530

    updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ export const environment = {
   production: true,
   BASE_API_URL: 'https://meghalayapradash.in/alpha_api/public/api',
   // BASE_API_URL: '/alpha_project/alpha_api/public/api',
-  BASE_PUBLIC_URL: '/alpha_project/alpha_api/public',
+  BASE_PUBLIC_URL: 'https://meghalayapradash.in/alpha_api/public',
+  // BASE_PUBLIC_URL: '/alpha_project/alpha_api/public',
 };
