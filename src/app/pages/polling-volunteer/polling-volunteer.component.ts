454a7e2e885ebe221d5147b7727aaa1d00d13584
priyamghosh1 <priyamghosh73@gmail.com>
2023-01-09 11:57:36 +0530

    vounteer updated

--- src/app/pages/polling-volunteer/polling-volunteer.component.ts
+++ src/app/pages/polling-volunteer/polling-volunteer.component.ts
@@ -325,37 +325,38 @@ export class PollingVolunteerComponent implements OnInit {
 
         const formData = new FormData();
         // @ts-ignore
-        formData.append("personTypeId", 8);
+        formData.append("personTypeId", 9);
         formData.append("personName", personFormData.personName);
         formData.append("age", personFormData.age);
         formData.append("gender", personFormData.gender);
-        formData.append("religion", personFormData.religion);
-        formData.append("occupation", personFormData.occupation);
-        formData.append("policeStation", personFormData.policeStation);
-        formData.append("cast", personFormData.cast);
-        formData.append("partNo", personFormData.partNo);
-        formData.append("postOffice", personFormData.postOffice);
-        formData.append("houseNo", personFormData.houseNo);
-        formData.append("guardianName", personFormData.guardianName);
-        formData.append("district", personFormData.district);
-        formData.append("pinCode", personFormData.pinCode);
-        formData.append("preferableCandidate", personFormData.preferableCandidate);
-        formData.append("satisfiedByPresentGov", personFormData.satisfiedByPresentGov);
-        formData.append("suggestion", personFormData.suggestion);
-        formData.append("previousVotingHistory", personFormData.prevVotingHistory);
         formData.append("email", personFormData.email);
-        formData.append("aadharId", personFormData.aadharId);
+        // formData.append("religion", personFormData.religion);
+        // formData.append("occupation", personFormData.occupation);
+        // formData.append("policeStation", personFormData.policeStation);
+        // formData.append("cast", personFormData.cast);
+        // formData.append("partNo", personFormData.partNo);
+        // formData.append("postOffice", personFormData.postOffice);
+        // formData.append("houseNo", personFormData.houseNo);
+        // formData.append("guardianName", personFormData.guardianName);
+        // formData.append("district", personFormData.district);
+        // formData.append("pinCode", personFormData.pinCode);
+        // formData.append("preferableCandidate", personFormData.preferableCandidate);
+        // formData.append("satisfiedByPresentGov", personFormData.satisfiedByPresentGov);
+        // formData.append("suggestion", personFormData.suggestion);
+        // formData.append("previousVotingHistory", personFormData.prevVotingHistory);
+        // formData.append("email", personFormData.email);
+        // formData.append("aadharId", personFormData.aadharId);
         // @ts-ignore
         formData.append("password", passwordMd5);
-        formData.append("mobile1", personFormData.mobile1);
-        formData.append("mobile2", personFormData.mobile2);
-        formData.append("voterId", personFormData.voterId);
-        formData.append("pollingStationId", personFormData.pollingStationId);
+        // formData.append("mobile1", personFormData.mobile1);
+        // formData.append("mobile2", personFormData.mobile2);
+        // formData.append("voterId", personFormData.voterId);
+        // formData.append("pollingStationId", personFormData.pollingStationId);
         // @ts-ignore
         formData.append("parentId", this.loggedInUser?.uniqueId);
-        formData.append("remark", this.userForm.value.remark);
-        formData.append("roadName", personFormData.roadName);
-        formData.append("district", personFormData.district);
+        // formData.append("remark", this.userForm.value.remark);
+        // formData.append("roadName", personFormData.roadName);
+        // formData.append("district", personFormData.district);
         // formData.append("state", personFormData.state);
 
         // formData.append("file", this.file);
