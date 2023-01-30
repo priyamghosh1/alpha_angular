47e75b54ca20a92be3bf67cd810b813e64efe296
mounitbhandari <tataibhandari@gmail.com>
2023-01-30 19:51:13 +0530

    updated legislaive steps

--- src/app/pages/legislative/legislative.component.ts
+++ src/app/pages/legislative/legislative.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import {UntypedFormBuilder, UntypedFormControl, UntypedFormGroup, Validators} from "@angular/forms";
-import {PollingMember} from "../../models/PollingMember";
-import {User} from "../../models/user.model";
-import {UserRegistrationService} from "../../services/user-registration.service";
-import {AuthService} from "../../services/auth.service";
-import {PollingStationService} from "../../services/polling-station.service";
-import {AreaService} from "../../services/area.service";
+import { UntypedFormBuilder, UntypedFormControl, UntypedFormGroup, Validators } from "@angular/forms";
+import { PollingMember } from "../../models/PollingMember";
+import { User } from "../../models/user.model";
+import { UserRegistrationService } from "../../services/user-registration.service";
+import { AuthService } from "../../services/auth.service";
+import { PollingStationService } from "../../services/polling-station.service";
+import { AreaService } from "../../services/area.service";
 import Swal from "sweetalert2";
-import {Md5} from "ts-md5";
-import {PollingVolunteer} from "../../models/PollingVolunteer";
+import { Md5 } from "ts-md5";
+import { PollingVolunteer } from "../../models/PollingVolunteer";
 import { LegislativeService } from 'src/app/services/legislative.service';
+import { LegendVolunteerService } from 'src/app/services/legend-volunteer.service';
 
 @Component({
   selector: 'app-legislative',
@@ -58,17 +59,71 @@ export class LegislativeComponent implements OnInit {
 
   loggedInUser: User | undefined;
 
-  legengVolunteers: any[]=[];
+  legengVolunteers: any[] = [];
   isUpdateAble = false;
 
+  legend: any[] = [];
+  districtAdmins: any[] = [];
+  assemblyVolunteer: any[] = [];
+  PolingAgentByAssembly: any[] = [];
+  boothVolunteerByPolingAgent: any[] = [];
+  volunteer: any[] = [];
+  votersList: any[] = [];
 
-  constructor(private authService: AuthService, private legislativeService: LegislativeService) { }
+
+  showLedgend = true;
+  showDistrictAdmin = false;
+  showAssemblyVolunteer = false;
+  showPollingVolunteer = false;
+  showBoothVolunteer = false;
+  showVolunteer = false;
+  showVoters = false;
+
+
+
+  showPhoto = true;
+  showName = true;
+  showMemberCode = false;
+  showGuardianName = true;
+  showAge = true;
+  showGender = true;
+  showPoliceStation = false;
+  showOcupation = true;
+  showReligion = true;
+  showEmail = false;
+  showMobileOne = true;
+  showMobileTwo = false;
+  showVoterId = true;
+  showAadharId = true;
+  showCast = false;
+  showPostOffice = false;
+  showHouseNo = true;
+  showRoadNo = true;
+  showPinCode = false;
+  showPollingNumber = false;
+
+
+  imageSrc: string | ArrayBuffer | null = "";
+  imageSrcVoter: string | ArrayBuffer | null = "";
+  imageSrcTable: string | ArrayBuffer | null = "";
+  defaultPicture: string = "";
+
+  file: File;
+
+  showBill = false;
+
+
+
+  constructor(private authService: AuthService, private legislativeService: LegislativeService,) {
+    // @ts-ignore
+    this.file = File;
+  }
 
   ngOnInit(): void {
-    this.loggedInUser = this.authService.userBehaviorSubject.value;   
+    this.loggedInUser = this.authService.userBehaviorSubject.value;
 
     this.legislativeService.getLegendByLegislative(this.loggedInUser.uniqueId).subscribe((response) => {
-      this.legengVolunteers= response.data;
+      this.legengVolunteers = response.data;
       // console.log(this.districtAdmins);
     });
 
@@ -211,6 +266,169 @@ export class LegislativeComponent implements OnInit {
   }
 
 
-  
+
+  getDistrictAdminByLegend(legendData: any) {
+
+    this.showDistrictAdmin = true;
+    this.showLedgend = false;
+    // console.log(legendData);
+
+    this.legislativeService.getDistrictAdminByLegend(legendData.id).subscribe((response) => {
+      this.districtAdmins = response.data;
+      // console.log(this.districtAdmins);
+    });
+  }
+
+  getAssemblyByDistrictAdmin(districtAdminData: any) {
+    // console.log(districtAdminData);
+    this.showDistrictAdmin = false;
+    this.showAssemblyVolunteer = true;
+
+    this.legislativeService.getAssemblyVolunteerByDistrictAdmin(districtAdminData.id).subscribe((response: any) => {
+      this.assemblyVolunteer = response.data;
+      // console.log(this.assemblyVolunteer);
+    });
+  }
+
+  getPollingMemberByAssembly(AssemblyData: any) {
+    // console.log(AssemblyData);
+
+    this.showAssemblyVolunteer = false;
+    this.showPollingVolunteer = true;
+
+    this.legislativeService.getPolingAgentByAssembly(AssemblyData.id).subscribe((response: any) => {
+      this.PolingAgentByAssembly = response.data;
+      // console.log("PolingAgentByAssembly", this.PolingAgentByAssembly)
+    })
+  }
+
+  getBoothVolunteerByPollingMember(pollingMemberData: any) {
+    // console.log(pollingMemberData);
+    this.showBoothVolunteer = true;
+    this.showPollingVolunteer = false;
+
+    this.legislativeService.getBoothByPolingAgent(pollingMemberData.id).subscribe((response: any) => {
+      this.boothVolunteerByPolingAgent = response.data;
+      // console.log("Booth", this.volunteerByPolingAgent)
+    })
+  }
+
+  getVolunteerByBoothVolunteer(boothMemberData: any) {
+    // console.log(boothMemberData);
+
+    this.showVolunteer = true;
+    this.showBoothVolunteer = false;
+
+    this.legislativeService.getVolunteerByBoothMember(boothMemberData.userId).subscribe((response: any) => {
+      this.volunteer = response.data;
+    })
+  }
+
+  getVotersByVolunteer(volunteerData: any) {
+    // console.log(volunteerData);
+
+    this.showVoters = true;
+    this.showVolunteer = false;
+
+    this.legislativeService.getAllvotersByUserId(volunteerData.userId).subscribe((response: { status: string, message: string, data: PollingMember[] }) => {
+      this.votersList = response.data;
+    });
+  }
+
+  changeShowStatus(x: any, y: any) {
+    this.showName = x === 'showName' ? y : this.showName;
+    this.showAge = x === 'showAge' ? y : this.showAge;
+    this.showEmail = x === 'showEmail' ? y : this.showEmail;
+    this.showMemberCode = x === 'showMemberCode' ? y : this.showMemberCode;
+    this.showGuardianName = x === 'showGuardianName' ? y : this.showGuardianName;
+    this.showGender = x === 'showGender' ? y : this.showGender;
+    this.showPoliceStation = x === 'showPoliceStation' ? y : this.showPoliceStation;
+    this.showOcupation = x === 'showOcupation' ? y : this.showOcupation;
+    this.showReligion = x === 'showReligion' ? y : this.showReligion;
+    this.showMobileOne = x === 'showMobileOne' ? y : this.showMobileOne;
+    this.showMobileTwo = x === 'showMobileTwo' ? y : this.showMobileTwo;
+    this.showVoterId = x === 'showVoterId' ? y : this.showVoterId;
+    this.showAadharId = x === 'showAadharId' ? y : this.showAadharId;
+    this.showCast = x === 'showCast' ? y : this.showCast;
+    this.showPostOffice = x === 'showPostOffice' ? y : this.showPostOffice;
+    this.showHouseNo = x === 'showHouseNo' ? y : this.showHouseNo;
+    this.showRoadNo = x === 'showRoadNo' ? y : this.showRoadNo;
+    this.showPinCode = x === 'showPinCode' ? y : this.showPinCode;
+    this.showPollingNumber = x === 'showPollingNumber' ? y : this.showPollingNumber;
+    this.showPhoto = x === 'showPhoto' ? y : this.showPhoto;
+
+
+  }
+
+
+  // showLedgend = true;
+  // showDistrictAdmin = false;
+  // showAssemblyVolunteer = false;
+  // showPollingVolunteer = false;
+  // showBoothVolunteer = false;
+  // showVolunteer = false;
+  // showVoters = false;
+
+  onClickBackButton() {
+    if (this.showDistrictAdmin == true) {
+      this.showLedgend = true;
+      this.showDistrictAdmin= false;
+      this.showAssemblyVolunteer = false;
+      this.showPollingVolunteer = false;
+      this.showBoothVolunteer = false;
+      this.showVolunteer = false;
+      this.showVoters = false;
+    }
+
+    if (this.showAssemblyVolunteer == true){      
+      this.showDistrictAdmin= true;
+      this.showLedgend = false;
+      this.showAssemblyVolunteer = false;
+      this.showPollingVolunteer = false;
+      this.showBoothVolunteer = false;
+      this.showVolunteer = false;
+      this.showVoters = false;
+    }
+
+    if (this.showPollingVolunteer == true){      
+      this.showDistrictAdmin= false;
+      this.showLedgend = false;
+      this.showAssemblyVolunteer = true;
+      this.showPollingVolunteer = false;
+      this.showBoothVolunteer = false;
+      this.showVolunteer = false;
+      this.showVoters = false;
+    }
+
+    if (this.showBoothVolunteer == true){      
+      this.showDistrictAdmin= false;
+      this.showLedgend = false;
+      this.showAssemblyVolunteer = false;
+      this.showPollingVolunteer = true;
+      this.showBoothVolunteer = false;
+      this.showVolunteer = false;
+      this.showVoters = false;
+    }
+
+    if (this.showVolunteer == true){      
+      this.showDistrictAdmin= false;
+      this.showLedgend = false;
+      this.showAssemblyVolunteer = false;
+      this.showPollingVolunteer = false;
+      this.showBoothVolunteer = true;
+      this.showVolunteer = false;
+      this.showVoters = false;
+    }
+
+    if (this.showVoters == true){      
+      this.showDistrictAdmin= false;
+      this.showLedgend = false;
+      this.showAssemblyVolunteer = false;
+      this.showPollingVolunteer = false;
+      this.showBoothVolunteer = false;
+      this.showVolunteer = true;
+      this.showVoters = false;
+    }
+  }
 
 }
--- src/app/services/legislative.service.ts
+++ src/app/services/legislative.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { catchError, Subject, tap } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { PollingMember } from '../models/PollingMember';
 import { ErrorService } from './error.service';
 
 @Injectable({
@@ -14,6 +15,24 @@ export class LegislativeService {
   votersSubject = new Subject<any[]>();
   legendVolunteersSubject = new Subject<any[]>();
   legendVolunteers: any[] = [];
+
+  districtVolunteers: any[] = [];
+  districtVolunteersSubject = new Subject<any[]>();
+
+  assemblyVolunteer: any[] = [];
+  assemblyVolunteerSubject = new Subject<any[]>();
+
+  pollingMembers: PollingMember[] = [];
+  pollingMemberSubject = new Subject<any[]>();
+
+  boothMemberSubject = new Subject<any[]>();
+  boothMembers: any[] = [];
+
+  volunteers: any[] = [];
+  volunteerSubject = new Subject<any[]>();
+
+  voterMembers: any[] = [];
+  voterMembersSubject = new Subject<any[]>();
 
   constructor(private http: HttpClient, private errorService: ErrorService) {
 
@@ -29,29 +48,29 @@ export class LegislativeService {
       }));
   }
 
-  updateLegendByLegislative(userData: any){
-    return this.http.put<{status:boolean, message:string ,data:any}>(this.BASE_API_URL + '/legendVolunteer', userData)
-    .pipe(catchError(this.errorService.serverError), tap(response => {
-      const x = this.legendVolunteers.findIndex(x=> x.personId === response.data.id);
-      this.legendVolunteers[x] = response.data;
-      this.legendVolunteersSubject.next([...this.legendVolunteers]);
-    }));
+  updateLegendByLegislative(userData: any) {
+    return this.http.put<{ status: boolean, message: string, data: any }>(this.BASE_API_URL + '/legendVolunteer', userData)
+      .pipe(catchError(this.errorService.serverError), tap(response => {
+        const x = this.legendVolunteers.findIndex(x => x.personId === response.data.id);
+        this.legendVolunteers[x] = response.data;
+        this.legendVolunteersSubject.next([...this.legendVolunteers]);
+      }));
   }
 
-  getLegendByLegislative(legislativeId: number){
-    return this.http.get<{status:string,message:string,data:any[]}>(this.BASE_API_URL + '/legendVolunteer/'+ legislativeId)
+  getLegendByLegislative(legislativeId: number) {
+    return this.http.get<{ status: string, message: string, data: any[] }>(this.BASE_API_URL + '/legendVolunteer/' + legislativeId)
       .pipe(catchError(this.errorService.serverError),
-        tap((response : {status:string,message:string,data:any[]}) => {
+        tap((response: { status: string, message: string, data: any[] }) => {
           this.legendVolunteers = response.data;
           this.legendVolunteersSubject.next([...this.legendVolunteers]);
         }));
   }
 
-  getLegendByLegislativeListener(){
+  getLegendByLegislativeListener() {
     return this.legendVolunteersSubject.asObservable();
   }
 
-  getvotersByLegislative(legislativeId: number){
+  getvotersByLegislative(legislativeId: number) {
 
     return this.http.get<{ status: string, message: string, data: any[] }>(this.BASE_API_URL + '/legendVolunteer/' + legislativeId + '/members')
       .pipe(catchError(this.errorService.serverError),
@@ -63,7 +82,104 @@ export class LegislativeService {
 
   }
 
-  getvotersByLegislativeListener(){
+  getvotersByLegislativeListener() {
     return this.votersSubject.asObservable();
   }
+
+
+
+  getDistrictAdminByLegend(legendVolunteerId: number) {
+    return this.http.get<{ status: string, message: string, data: any[] }>(this.BASE_API_URL + '/districtAdmin/' + legendVolunteerId)
+      .pipe(catchError(this.errorService.serverError),
+        tap((response: { status: string, message: string, data: any[] }) => {
+          this.districtVolunteers = response.data;
+          this.districtVolunteersSubject.next([...this.districtVolunteers]);
+        }));
+  }
+
+  getDistrictAdminByLegendListener() {
+    return this.districtVolunteersSubject.asObservable();
+  }
+
+
+
+  getAssemblyVolunteerByDistrictAdmin(districtAdminId: number) {
+    return this.http.get<{ status: string, message: string, data: any[] }>(this.BASE_API_URL + '/assemblyVolunteer/' + districtAdminId)
+      .pipe(catchError(this.errorService.serverError),
+        tap((response: { status: string, message: string, data: any[] }) => {
+          this.assemblyVolunteer = response.data;
+          this.assemblyVolunteerSubject.next([...this.assemblyVolunteer]);
+          // console.log(this.assemblyVolunteer);
+        }));
+  }
+  getAssemblyVolunteerByDistrictAdminListener() {
+    return this.assemblyVolunteerSubject.asObservable();
+  }
+
+
+  getPolingAgentByAssembly(assemblyId: number) {
+    // return  this.http.get(this.BASE_API_URL + '/volunteer/' + pollingAgentId).subscribe((response: ServerResponse) => {
+    //   console.log(response);
+    // });
+
+    return this.http.get<{ status: string, message: string, data: PollingMember[] }>(this.BASE_API_URL + '/pollingVolunteer/' + assemblyId)
+      .pipe(catchError(this.errorService.serverError),
+        tap((response: { status: string, message: string, data: any }) => {
+          this.pollingMembers.unshift(response.data);
+          this.pollingMemberSubject.next([...this.pollingMembers]);
+        }));
+  }
+
+
+  getPolingAgentByAssemblyListener() {
+    return this.pollingMemberSubject.asObservable();
+  }
+
+
+  getBoothByPolingAgent(pollingAgentId: number) {
+    // return  this.http.get(this.BASE_API_URL + '/volunteer/' + pollingAgentId).subscribe((response: ServerResponse) => {
+    //   console.log(response);
+    // });
+
+    return this.http.get<{ status: string, message: string, data: any[] }>(this.BASE_API_URL + '/boothVolunteer/' + pollingAgentId)
+      .pipe(catchError(this.errorService.serverError),
+        tap((response: { status: string, message: string, data: any[] }) => {
+          this.boothMembers.unshift(response.data);
+          this.boothMemberSubject.next([...this.boothMembers]);
+        }));
+  }
+
+
+
+
+  getVolunteerByBoothMember(pollingAgentId: number) {
+    // return  this.http.get(this.BASE_API_URL + '/volunteer/' + pollingAgentId).subscribe((response: ServerResponse) => {
+    //   console.log(response);
+    // });
+
+    return this.http.get<{ status: string, message: string, data: any[] }>(this.BASE_API_URL + '/volunteer/' + pollingAgentId)
+      .pipe(catchError(this.errorService.serverError),
+        tap((response: { status: string, message: string, data: any[] }) => {
+          // console.log(response);
+          this.volunteers.unshift(response.data);
+          this.volunteerSubject.next([...this.volunteers]);
+        }));
+  }
+
+
+  getAllvotersByUserId(userId:number):any{
+
+    return this.http.get<{status:string,message:string,data:any[]}>(this.BASE_API_URL + '/volunteer/'+ userId + '/members')
+      .pipe(catchError(this.errorService.serverError),
+        tap((response : {status:string,message:string,data:any[]}) => {
+          this.voterMembers = response.data;
+          this.voterMembersSubject.next([...this.voterMembers]);
+        }));
+
+  }
+  getAllVoterListener(){
+    return this.voterMembersSubject.asObservable();
+  }
+
+
 }
